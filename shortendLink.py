53d605feedb622fa9bbffcebb148e79a3163ce3b
owaish <owaish3301@gmail.com>
2024-06-20 15:56:44 +0530

    final commit

--- shortendLink.py
+++ shortendLink.py
@@ -5,7 +5,7 @@ def short_url(url):
         # Create a shortened URL
         response = requests.get("https://instantearn.in/api", params={"api": "4bd5334c8bbcd2ea1da78e99fefa7b48bbff377d", "url": url})
         response.raise_for_status()  # Raise an exception if the request was unsuccessful
-        shortened_url = response.json()["shortenedUrl"]
+        shortened_url = response
         return shortened_url
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
